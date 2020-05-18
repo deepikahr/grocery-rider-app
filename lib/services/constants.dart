70469f283a46b5d2a0c1153fca049f73c68bd2f4
Sandip Kakadiya <Sandip@pietechsolution.com>
2020-05-18 11:18:22 +0530

    google api key changes

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -1,15 +1,15 @@
 class Constants {
   // delopy url production
-  // static const BASE_URL = "http://162.243.171.81:3000/";
+  static const BASE_URL = "http://162.243.171.81:3000/";
 
   // delopy url testing
-  static const BASE_URL = "http://162.243.171.81:4000/";
+  // static const BASE_URL = "http://162.243.171.81:4000/";
 
   // static const BASE_URL = 'http://63fb5cba.ngrok.io/';
   // ONE_SIGNAL_KEY
   static const ONE_SIGNAL_KEY = "a76b6867-bf12-440f-8bc2-30946c652af8";
   // Google API key
-  static const GOOGLE_API_KEY = "AIzaSyDXxt_aIn5HWQZg3gFYOqcuf8hjUuzmvKg";
+  static const GOOGLE_API_KEY = "AIzaSyD6Q4UgAYOL203nuwNeBr4j_-yAd1U1gko";
   // URL for socket usage
   static final String socketUrl = BASE_URL.substring(0, BASE_URL.length - 1);
 }
