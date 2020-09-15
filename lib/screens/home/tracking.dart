9739f26d0ab53805f68fb1b70e65f60db4916baa
vipul8815 <vipul@pietechsolution.com>
2020-09-15 17:54:31 +0530

    add discount key and fixed button issue

--- lib/screens/home/tracking.dart
+++ lib/screens/home/tracking.dart
@@ -348,6 +348,7 @@ class _TrackingState extends State<Tracking> {
                   buildPaymentInfoBlock(),
                   SizedBox(height: 20),
                   buildDeliveredButton(),
+                  SizedBox(height: 300),
                 ],
               ),
             ),
@@ -597,6 +598,10 @@ class _TrackingState extends State<Tracking> {
                     ? Container()
                     : orderSummary(context, "DELIVERY_CHARGES",
                         "$currency${order['cart']['deliveryCharges'].toDouble().toStringAsFixed(2)}"),
+                order['cart']['couponAmount'] == 0
+                    ? Container()
+                    : orderSummary(context, "DISCOUNT",
+                        "$currency${order['cart']['couponAmount'].toDouble().toStringAsFixed(2)}"),
                 orderSummary(context, "TOTAL",
                     "$currency${order['cart']['grandTotal'].toDouble().toStringAsFixed(2)}"),
               ],
